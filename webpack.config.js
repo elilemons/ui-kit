34e322a43d34ecec0b74ca46e07aa56ff63a2234
Lisa Lemons <me@lisa-lemons.com>
2018-06-25 20:22:51 -0400

    Installing webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'UI Kit'
+        }),
+    ]
+};
